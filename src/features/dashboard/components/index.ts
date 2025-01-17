287c7e5105fc9a90c409c8e141a3213b26de4ab6
ebegcoding <aleksacollaku34@gmail.com>
2025-01-17 23:22:20 +0100

    feat: lazy load charts

--- src/features/dashboard/components/index.ts
+++ src/features/dashboard/components/index.ts
@@ -2,6 +2,3 @@ export * from "./dashboard-card";
 export * from "./transactions-card";
 export * from "./contacts-card";
 export * from "./credit-cards-card";
-export * from "./activity-card";
-export * from "./balance-card";
-export * from "./expenses-card";
--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,22 +1,38 @@
-import {
-  ActivityCard,
-  BalanceCard,
-  ContactsCard,
-  CreditCardsCard,
-  ExpensesCard,
-  TransactionsCard,
-} from "./components";
+import { lazy, Suspense } from "react";
+import { ContactsCard, CreditCardsCard, TransactionsCard } from "./components";
 import { StyledContainer } from "./dashboard.styles";
+
+const BalanceCard = lazy(() =>
+  import("./components/balance-card/balance-card").then((module) => ({
+    default: module.BalanceCard,
+  }))
+);
+
+const ActivityCard = lazy(() =>
+  import("./components/activity-card/activity-card").then((module) => ({
+    default: module.ActivityCard,
+  }))
+);
+
+const ExpensesCard = lazy(() =>
+  import("./components/expenses-card/expenses-card").then((module) => ({
+    default: module.ExpensesCard,
+  }))
+);
 
 export const Dashboard = () => {
   return (
     <StyledContainer>
       <CreditCardsCard />
       <TransactionsCard />
-      <ActivityCard />
-      <ExpensesCard />
+      <Suspense>
+        <ActivityCard />
+        <ExpensesCard />
+      </Suspense>
       <ContactsCard />
-      <BalanceCard />
+      <Suspense>
+        <BalanceCard />
+      </Suspense>
     </StyledContainer>
   );
 };
